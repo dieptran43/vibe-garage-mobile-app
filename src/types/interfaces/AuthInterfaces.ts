66349191242f73e21ebf867259cf3b7438a5ef72
Princewill Iroka <princewilliroka@outlook.com>
2020-12-17 18:19:59 +0100

    Added AuthInterfaces to authServices

---
+++ src/types/interfaces/AuthInterfaces.ts
@@ -0,0 +1,22 @@
+import {EAccountType} from '../enums';
+
+export interface ILogin {
+  email: String;
+  password: String;
+}
+
+export interface IRegistration {
+  username: String;
+  email: String;
+  password: String;
+  name: String;
+  account_type: EAccountType;
+}
+
+export interface IActivateAccount {
+  code: String;
+}
+
+export interface IToken {
+  token: String;
+}
