b254e02f6388a6e29bd5c263a471ade360d29697
Princewill Iroka <princewilliroka@outlook.com>
2021-01-01 23:08:59 +0100

    Updated Carousel content

--- src/views/Tour/Discover/index.tsx
+++ src/views/Tour/Discover/index.tsx
@@ -8,7 +8,7 @@ import {
   TouchableOpacity,
   KeyboardAvoidingView,
   ScrollView,
-  Dimensions
+  Dimensions,
 } from 'react-native';
 import Carousel from 'react-native-snap-carousel';
 import {DrawerScreenProps} from '@react-navigation/drawer';
@@ -21,49 +21,35 @@ const windowWidth = Dimensions.get('window').width;
 
 export function Discover({navigation}: DrawerScreenProps<{}>) {
   const [data, setData] = useState({
-    sliderImages: [
-      'https://musicport.com.ng/upload/photos/2019/10/NuJHxVutEiIgT86CcFqr_06_6dfa801e8668b414c32232e7c8b93426_image.jpg',
-      'https://musicport.com.ng/upload/photos/2019/10/J7qytBWEsADF5zBsC6Os_17_98885479029d3e547509c8e8ea8d1e3b_image.jpg',
-      'https://musicport.com.ng/upload/photos/2019/11/wbWF1rYguTq8JtKZIaM6_22_a47c1c30c1e7318a49df8de5748a9ea7_image.jpeg',
-      'https://musicport.com.ng/upload/photos/2020/01/MUDLnD9cXKhvkVJuUGAK_29_0c0801563af4b03dea742996a6cde2e0_image.png',
-    ],
     carouselItems: [
       {
-        title: 'Item 1',
-        text: 'Text 1',
+        title: 'October special',
+        image:
+          'https://musicport.com.ng/upload/photos/2019/10/NuJHxVutEiIgT86CcFqr_06_6dfa801e8668b414c32232e7c8b93426_image.jpg',
       },
       {
-        title: 'Item 2',
-        text: 'Text 2',
+        title: 'LABIS BOY_MAN OF THE YEAR.mp3',
+        image:
+          'https://musicport.com.ng/upload/photos/2019/10/J7qytBWEsADF5zBsC6Os_17_98885479029d3e547509c8e8ea8d1e3b_image.jpg',
       },
       {
-        title: 'Item 3',
-        text: 'Text 3',
+        title: 'November TOP 5',
+        image:
+          'https://musicport.com.ng/upload/photos/2019/11/wbWF1rYguTq8JtKZIaM6_22_a47c1c30c1e7318a49df8de5748a9ea7_image.jpeg',
       },
       {
-        title: 'Item 4',
-        text: 'Text 4',
-      },
-      {
-        title: 'Item 5',
-        text: 'Text 5',
+        title: 'Exhausted (I don tire).mp3',
+        image:
+          'https://musicport.com.ng/upload/photos/2020/01/MUDLnD9cXKhvkVJuUGAK_29_0c0801563af4b03dea742996a6cde2e0_image.png',
       },
     ],
   });
 
   const _renderItem = ({item}: any) => {
     return (
-      <View
-        style={{
-          backgroundColor: 'floralwhite',
-          borderRadius: 5,
-          height: 250,
-          padding: 50,
-          marginLeft: 25,
-          marginRight: 25,
-        }}>
-        <Text style={{fontSize: 30}}>{item.title}</Text>
-        <Text>{item.text}</Text>
+      <View style={styles.carouselContainer}>
+        <Image source={{uri: item?.image}} style={styles.carouselImage} />
+        <Text style={styles.carouselText}>{item.title}</Text>
       </View>
     );
   };
